fbd5135471eb2d8370f1ddd3a91be6078c8a036c
Thais49 <t.freire@escola.pr.gov.br>
2025-11-05 11:37:54 -0300

    mudanças no paciente

--- js/principal.js
+++ js/principal.js
@@ -2,3 +2,14 @@ var titulo = document.querySelector(".título");
 titulo.textContent = "Aparecida Nutricionista";
 		
 /*  IMC - massa/altura^2    */
+var paciente = document.querySelector("#primeiro-paciente");
+var tdpeso = document.querySelector(".info-peso");
+var peso = tdpeso.textContent;
+var tdaltura = document.querySelector(".info-altura")
+var altura = tdaltura.textContent;
+var imc = peso / (altura*altura);
+
+console.log(paciente); //tr
+console.log(tdpeso); //td que tem peso
+console.log(peso); //vaor do peso
+console.log(altura); //valor da altura
